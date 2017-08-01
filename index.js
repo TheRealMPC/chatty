d9e41d3623ffaf1cb46acd828ac709ffd455b4e5
TheRealMPC <smkscar25@hotmail.com>
2017-08-01 16:38:37 -0500

    Chatty Complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+var app = express();
+
+var messages = [];
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+
+app.listen(3000, function() {
+  console.log("listening on port 3000");
+});
